ff7e77be10cb9c0930917862bd762027449a1152
Patrick Negri <patrick@iugu.com.br>
2012-10-06 10:53:06 -0300

    [ ! ] Working on Small Screens

--- lib/iugu-ux/engine.rb
+++ lib/iugu-ux/engine.rb
@@ -6,7 +6,7 @@ module IuguUX
       app.config.assets.precompile += %w( iugu-ux.css iugu-ux.js vendor.js )
       # app.config.compass.sprite_load_path << File.join( File.expand_path("../../", __FILE__),'vendor','assets','sprites' )
       app.config.compass.sprite_load_path << app.root.join('app','assets','sprites')
-      app.config.compass.sprite_load_path << File.join( File.expand_path("../../",__FILE__),'vendor','assets','sprites' )
+      app.config.compass.sprite_load_path << File.join( File.expand_path("../../..",__FILE__),'vendor','assets','sprites' )
       # app.config.compass.sprite_load_path << 'app/assets/sprites'
       # app.config.compass.sprite_load_path << 'vendor/assets/sprites'
     end
