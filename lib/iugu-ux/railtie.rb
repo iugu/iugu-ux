40dae59eec78b46ee40b7f13022a73899adeffd1
Patrick Negri <patrick@iugu.com.br>
2012-11-30 03:26:55 -0200

    [ ! ] More path errors

--- lib/iugu-ux/railtie.rb
+++ lib/iugu-ux/railtie.rb
@@ -4,11 +4,11 @@ require 'compass-rails'
 module IuguUX
   class Engine < Rails::Engine
       initializer 'iugu-ux.setup' do |app|
-        unless Rails.env.production?
-          app.config.assets.precompile += IuguUX.src
-          app.config.compass.sprite_load_path << app.root.join('app','assets','sprites')
-          app.config.compass.sprite_load_path << IuguUX.sprite_load_path 
-          app.config.compass.add_import_path File.join( IuguUX.assets_path, 'stylesheets' )
+        app.config.assets.precompile += IuguUX.src
+        app.config.compass.sprite_load_path << app.root.join('app','assets','sprites')
+        app.config.compass.sprite_load_path << IuguUX.sprite_load_path 
+        app.config.compass.add_import_path File.join( IuguUX.assets_path, 'stylesheets' )
+        if defined?(Sass)
           app.config.sass.load_paths << File.join( IuguUX.assets_path, 'stylesheets' )
           app.config.sass.load_paths << File.join( IuguUX.assets_path, 'stylesheets', 'iugu-ux' )
           app.config.sass.load_paths << File.join( IuguUX.assets_path, 'stylesheets', 'iugu-ux', 'google-code-prettify' )
