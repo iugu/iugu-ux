fea179ce9b076084892cb12a7dfb56a0e30ce392
Patrick Negri <patrick@iugu.com.br>
2012-11-30 03:20:54 -0200

    [ ! ] Prettify still holding up

--- lib/iugu-ux/railtie.rb
+++ lib/iugu-ux/railtie.rb
@@ -4,15 +4,11 @@ require 'compass-rails'
 module IuguUX
   class Engine < Rails::Engine
       initializer 'iugu-ux.setup' do |app|
-        if app.config.assets
+        unless Rails.env.production?
           app.config.assets.precompile += IuguUX.src
-        end
-        if app.config.compass
           app.config.compass.sprite_load_path << app.root.join('app','assets','sprites')
           app.config.compass.sprite_load_path << IuguUX.sprite_load_path 
           app.config.compass.add_import_path File.join( IuguUX.assets_path, 'stylesheets' )
-        end
-        if defined?(Saas)
           app.config.sass.load_paths << File.join( IuguUX.assets_path, 'stylesheets' )
           app.config.sass.load_paths << File.join( IuguUX.assets_path, 'stylesheets', 'iugu-ux' )
           app.config.sass.load_paths << File.join( IuguUX.assets_path, 'stylesheets', 'iugu-ux', 'google-code-prettify' )
