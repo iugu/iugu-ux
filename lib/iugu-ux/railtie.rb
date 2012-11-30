cb991817721c39f43286da0af172bf8f358703eb
Patrick Negri <patrick@iugu.com.br>
2012-11-30 02:04:36 -0200

    [ ! ] Dont try to run SaaS and Compass in Production

--- lib/iugu-ux/railtie.rb
+++ lib/iugu-ux/railtie.rb
@@ -4,13 +4,15 @@ require 'compass-rails'
 module IuguUX
   class Engine < Rails::Engine
       initializer 'iugu-ux.setup' do |app|
-        app.config.assets.precompile += IuguUX.src
-        app.config.compass.sprite_load_path << app.root.join('app','assets','sprites')
-        app.config.compass.sprite_load_path << IuguUX.sprite_load_path 
-        app.config.compass.add_import_path File.join( IuguUX.assets_path, 'stylesheets' )
-        app.config.sass.load_paths << File.join( IuguUX.assets_path, 'stylesheets' )
-        app.config.sass.load_paths << File.join( IuguUX.assets_path, 'stylesheets', 'iugu-ux' )
-        app.config.sass.load_paths << File.join( IuguUX.assets_path, 'stylesheets', 'iugu-ux', 'google-code-prettify' )
+        unless Rails.env.production?
+          app.config.assets.precompile += IuguUX.src
+          app.config.compass.sprite_load_path << app.root.join('app','assets','sprites')
+          app.config.compass.sprite_load_path << IuguUX.sprite_load_path 
+          app.config.compass.add_import_path File.join( IuguUX.assets_path, 'stylesheets' )
+          app.config.sass.load_paths << File.join( IuguUX.assets_path, 'stylesheets' )
+          app.config.sass.load_paths << File.join( IuguUX.assets_path, 'stylesheets', 'iugu-ux' )
+          app.config.sass.load_paths << File.join( IuguUX.assets_path, 'stylesheets', 'iugu-ux', 'google-code-prettify' )
+        end
       end
 
   end
