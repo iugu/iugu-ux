a8100da4a3702351950d9b238460d6fa2b872207
Patrick Negri <patrick@iugu.com.br>
2012-11-30 02:37:18 -0200

    [ ! ] Testing what gems are loaded: SaaS,Compass

--- lib/iugu-ux/railtie.rb
+++ lib/iugu-ux/railtie.rb
@@ -4,11 +4,15 @@ require 'compass-rails'
 module IuguUX
   class Engine < Rails::Engine
       initializer 'iugu-ux.setup' do |app|
-        unless Rails.env.production?
+        if app.consig.assets
           app.config.assets.precompile += IuguUX.src
+        end
+        if app.config.compass
           app.config.compass.sprite_load_path << app.root.join('app','assets','sprites')
           app.config.compass.sprite_load_path << IuguUX.sprite_load_path 
           app.config.compass.add_import_path File.join( IuguUX.assets_path, 'stylesheets' )
+        end
+        if app.config.sass
           app.config.sass.load_paths << File.join( IuguUX.assets_path, 'stylesheets' )
           app.config.sass.load_paths << File.join( IuguUX.assets_path, 'stylesheets', 'iugu-ux' )
           app.config.sass.load_paths << File.join( IuguUX.assets_path, 'stylesheets', 'iugu-ux', 'google-code-prettify' )
