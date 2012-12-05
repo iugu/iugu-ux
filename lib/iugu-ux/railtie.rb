67be52538b4218deb193a2ea69caf3d3056d3f47
Patrick Negri <patrick@iugu.com.br>
2012-12-04 23:18:38 -0200

    [ ! ] Adding Initializer to :group => :all

--- lib/iugu-ux/railtie.rb
+++ lib/iugu-ux/railtie.rb
@@ -3,14 +3,20 @@ require 'compass-rails'
 
 module IuguUX
   class Engine < Rails::Engine
-      initializer 'iugu-ux.setup' do |app|
+      initializer 'iugu-ux.setup', :group => :all do |app|
         app.config.assets.precompile += IuguUX.src
         app.config.compass.sprite_load_path << app.root.join('app','assets','sprites')
         app.config.compass.sprite_load_path << IuguUX.sprite_load_path 
-        app.config.compass.add_import_path File.join( IuguUX.assets_path, 'stylesheets' )
-        app.config.sass.load_paths << File.join( IuguUX.assets_path, 'stylesheets' )
-        app.config.sass.load_paths << File.join( IuguUX.assets_path, 'stylesheets', 'iugu-ux' )
-        app.config.sass.load_paths << File.join( IuguUX.assets_path, 'stylesheets', 'iugu-ux', 'google-code-prettify' )
+        app.config.assets.paths.unshift File.join( IuguUX.assets_path, 'javascripts' )
+        app.config.assets.paths.unshift File.join( IuguUX.assets_path, 'javascripts', 'web-app' )
+        app.config.assets.paths.unshift File.join( IuguUX.assets_path, 'stylesheets' )
+        app.config.assets.paths.unshift File.join( IuguUX.assets_path, 'stylesheets', 'iugu-ux' )
+        app.config.assets.precompile << %w( vendor.js iugu-ux.css iugu-ux.js )
+
+        # app.config.compass.add_import_path File.join( IuguUX.assets_path, 'stylesheets' )
+        # app.config.sass.load_paths << File.join( IuguUX.assets_path, 'stylesheets' )
+        # app.config.sass.load_paths << File.join( IuguUX.assets_path, 'stylesheets', 'iugu-ux' )
+        # app.config.sass.load_paths << File.join( IuguUX.assets_path, 'stylesheets', 'iugu-ux', 'google-code-prettify' )
       end
 
   end
